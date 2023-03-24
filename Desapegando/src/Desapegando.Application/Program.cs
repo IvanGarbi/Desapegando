6e03754c2d7b2fa118fd5f3fe05595d81b689402
IvanGarbi <ivan_garbi@hotmail.com>
2023-03-24 13:58:36 -0300

    Program e CondominoValidation

--- Desapegando/src/Desapegando.Application/Program.cs
+++ Desapegando/src/Desapegando.Application/Program.cs
@@ -1,4 +1,7 @@
 using Desapegando.Application.Data;
+using Desapegando.Business.Interfaces.Services;
+using Desapegando.Business.Services;
+using Desapegando.Data.Context;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,12 +10,30 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+{
+    options.UseSqlServer(connectionString);
+
+});
+
+builder.Services.AddDbContext<DesapegandoDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+
+// Dependcy Injection
+builder.Services.AddScoped<ICondominoService, CondominoService>();
+
+
+// AutoMapper
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
---
+++ Desapegando/src/Desapegando.Business/Validations/CondominoValidation.cs
@@ -0,0 +1,75 @@
+﻿using Desapegando.Business.Models;
+using FluentValidation;
+
+namespace Desapegando.Business.Validations;
+
+public class CondominoValidation : AbstractValidator<Condomino>
+{
+    public CondominoValidation()
+    {
+        RuleFor(x => x.Sexo)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("O {PropertyName} deve ser informado.")
+            .IsInEnum();
+
+        RuleFor(x => x.Email)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("O {PropertyName} deve ser informado.")
+            .EmailAddress()
+            .WithMessage("O {PropertyName} está em formato inválido.")
+            .MaximumLength(100)
+            .WithMessage("O {PropertyName} deve ter menos que 100 caracteres.");
+
+        RuleFor(x => x.Sobrenome)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("O {PropertyName} deve ser informado.")
+            .MaximumLength(100)
+            .WithMessage("O {PropertyName} deve ter menos que 100 caracteres.");
+
+        RuleFor(x => x.Nome)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("O {PropertyName} deve ser informado.")
+            .MaximumLength(100)
+            .WithMessage("O {PropertyName} deve ter menos que 100 caracteres.");
+
+        RuleFor(x => x.Telefone)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("O {PropertyName} deve ser informado.")
+            .Matches("^[0-9]+$")
+            .WithMessage("O {PropertyName} deve conter somente números.")
+            .MaximumLength(20)
+            .WithMessage("O {PropertyName} deve ter menos que 20 caracteres.");
+
+        RuleFor(x => x.Apartamento)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("O {PropertyName} deve ser informado.")
+            .MaximumLength(20)
+            .WithMessage("O {PropertyName} deve ter menos que 20 caracteres."); ;
+
+        RuleFor(x => x.Idade)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("O {PropertyName} deve ser informado.");
+
+        RuleFor(x => x.Cpf)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("O {PropertyName} deve ser informado.")
+            .Matches("^[0-9]+$")
+            .WithMessage("O {PropertyName} deve conter somente números.")
+            .MinimumLength(11)
+            .MaximumLength(11)
+            .WithMessage("O {PropertyName} deve ter 11 caracteres.");
+
+        RuleFor(x => x.Administrador)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("O {PropertyName} deve ser informado.");
+    }
+}
