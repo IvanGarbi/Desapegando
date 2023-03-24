73aa882d36eb5fee2d4665fa7e74f9a0d930ec48
IvanGarbi <ivan_garbi@hotmail.com>
2023-03-24 13:57:32 -0300

    AutoMapper

---
+++ Desapegando/src/Desapegando.Application/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using System.Net;
+using Desapegando.Application.Models;
+using Desapegando.Business.Models;
+
+namespace Desapegando.Application.AutoMapper;
+
+public class AutoMapperConfig : Profile
+{
+    public AutoMapperConfig()
+    {
+        CreateMap<CondominoViewModel, Condomino>().ReverseMap();
+    }
+}
