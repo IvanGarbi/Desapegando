3b642f07e653af6d3516b38b1af45fcb39fd1cee
IvanGarbi <ivan_garbi@hotmail.com>
2023-03-24 13:59:38 -0300

    Csproj, enum na view e login controller update

--- Desapegando/src/Desapegando.Application/Controllers/LoginController.cs
+++ Desapegando/src/Desapegando.Application/Controllers/LoginController.cs
@@ -1,17 +1,58 @@
-﻿using Desapegando.Application.Models;
+﻿using AutoMapper;
+using Desapegando.Application.Models;
+using Desapegando.Business.Interfaces.Services;
+using Desapegando.Business.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Desapegando.Application.Controllers;
 
 public class LoginController : MainController
 {
-    public LoginController()
+    private readonly ICondominoService _condominoService;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly IMapper _mapper;
+
+    public LoginController(UserManager<IdentityUser> userManager, ICondominoService condominoService, SignInManager<IdentityUser> signInManager, IMapper mapper)
     {
-        
+        _userManager = userManager;
+        _condominoService = condominoService;
+        _signInManager = signInManager;
+        _mapper = mapper;
     }
 
     public async Task<IActionResult> Index()
     {
         return View();
     }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(CondominoViewModel condominoViewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(condominoViewModel);
+        }
+
+        var user = new IdentityUser();
+        user.Email = condominoViewModel.Email;
+        user.PhoneNumber = condominoViewModel.Telefone;
+        user.UserName = condominoViewModel.Nome;
+
+        var result = await _userManager.CreateAsync(user);
+
+        if (result.Succeeded)
+        {
+            var identity = await _userManager.FindByEmailAsync(condominoViewModel.Email);
+            var condomino = _mapper.Map<Condomino>(condominoViewModel);
+
+            condomino.Id = Guid.Parse(identity.Id);
+
+            await _condominoService.Create(condomino);
+        }
+
+        return View();
+    }
+
 }
