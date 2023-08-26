4c5efd3b109ae14b91c0935d2eea49c6269d0c62
Ivan Assumpcao <Ivan.Assumpcao@landisgyr.com>
2023-08-26 14:22:44 -0300

    Atualizando controller do condomino

--- Desapegando/src/Desapegando.Application/Controllers/CondominoController.cs
+++ Desapegando/src/Desapegando.Application/Controllers/CondominoController.cs
@@ -1,11 +1,6 @@
-﻿using AutoMapper;
-using Desapegando.Application.Extensions;
+﻿using Desapegando.Application.Extensions;
 using Desapegando.Application.ViewModels;
 using Desapegando.Business.Interfaces.Notifications;
-using Desapegando.Business.Interfaces.Repository;
-using Desapegando.Business.Interfaces.Services;
-using Desapegando.Business.Models;
-using Desapegando.Business.Validations;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -18,41 +13,21 @@ public class CondominoController : MainController
 {
     private readonly HttpClient _httpClient;
 
-    private readonly ICondominoRepository _condominoRepository;
-    private readonly ICondominoService _condominoService;
     private readonly UserManager<IdentityUser> _userManager;
-    private readonly IMapper _mapper;
 
     public CondominoController(HttpClient httpClient,
                                IOptions<AppSettings> settings,
-                               ICondominoRepository condominoRepository, 
-                               IMapper mapper, 
-                               UserManager<IdentityUser> userManager, 
-                               ICondominoService condominoService,
                                INotificador notificador) : base(notificador)
     {
-        _condominoRepository = condominoRepository;
-        _mapper = mapper;
-        _userManager = userManager;
-        _condominoService = condominoService;
-
         httpClient.BaseAddress = new Uri(settings.Value.DesapegandoApiUrl);
         _httpClient = httpClient;
     }
 
-    //public async Task<IActionResult> Index()
-    //{
-    //    var teste = await _condominoRepository.ReadById(Guid.Parse(_userManager.GetUserId(User)));
-
-    //    return View(_mapper.Map<CondominoViewModel>(await _condominoRepository.ReadById(Guid.Parse(_userManager.GetUserId(User)))));
-    //}
-
     public async Task<IActionResult> Index()
     {
-        var condomino = await _condominoRepository.ReadById(Guid.Parse(User.FindFirst("sub")?.Value));
+        var condominoId = Guid.Parse(User.FindFirst("sub")?.Value);
 
-
-        var response = await _httpClient.GetAsync("Condomino/Condomino/" + condomino.Id);
+        var response = await _httpClient.GetAsync("Condomino/Condomino/" + condominoId);
 
         GetCondominoResponseId employeesResponse;
 
@@ -60,32 +35,6 @@ public class CondominoController : MainController
 
         return View(employeesResponse.Data);
     }
-
-    //[HttpPost]
-    //public async Task<IActionResult> Index(CondominoViewModel condominoViewModel)
-    //{
-    //    if (!ModelState.IsValid)
-    //    {
-    //        return View(condominoViewModel);
-    //    }
-
-    //    var condomino = _mapper.Map<Condomino>(condominoViewModel);
-
-    //    var validator = new CondominoValidation();
-    //    var resultValidation = validator.Validate(condomino);
-
-    //    if (!resultValidation.IsValid)
-    //    {
-    //        foreach (var error in resultValidation.Errors)
-    //        {
-    //            ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
-    //        }
-    //    }
-
-    //    await _condominoService.Update(condomino);
-
-    //    return View(condominoViewModel);
-    //}
 
     [HttpPost]
     public async Task<IActionResult> Index(CondominoViewModel condominoViewModel)
