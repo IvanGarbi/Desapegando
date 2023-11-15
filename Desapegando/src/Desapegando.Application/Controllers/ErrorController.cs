e1e80b5735137b2e18047b76278962f10fd8a8d2
Ivan Assumpcao <Ivan.Assumpcao@landisgyr.com>
2023-11-14 22:00:44 -0300

    mudancas

---
+++ Desapegando/src/Desapegando.Application/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using Desapegando.Application.ViewModels;
+using Desapegando.Business.Interfaces.Notifications;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace Desapegando.Application.Controllers
+{
+    public class ErrorController : MainController
+    {
+        public ErrorController(INotificador notificador) : base(notificador)
+        {
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
