2dac391849cdcbba64cd90a5c15c86d97f95b96f
IvanGarbi <ivan_garbi@hotmail.com>
2024-01-23 21:41:52 -0300

    Teste inicial calendário reserva lavanderia

---
+++ Desapegando/src/Desapegando.Application/Controllers/LavanderiaController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Desapegando.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Desapegando.Application.Controllers
+{
+    public class LavanderiaController : MainController
+    {
+        private readonly IMapper _mapper;
+
+        public LavanderiaController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
